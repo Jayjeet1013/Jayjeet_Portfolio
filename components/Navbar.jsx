1957d3800321be86855554a8ed5025da95ae5062
Jayjeet1013 <jayjeet1013@gmail.com>
2023-03-03 17:42:11 +0530

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,29 @@ import Link from 'next/link';
 import React from 'react'
 
 const Navbar = () => {
+    const links = [
+        {
+            id:1,
+            link:'home',
+        },
+        {
+            id:2,
+            link:'portfolio',
+        },
+        {
+            id:3,
+            link:'experience',
+        },
+        {
+            id:4,
+            link:'me',
+        },
+        {
+            id:5,
+            link:'contact',
+        },
+    ];
+
   return (
     <div className='w-full h-20 z-10 fixed bg-white text-black duration-300 ease-in'>
      <div className='flex justify-between items-center w-full h-full max-w-screen-xl mx-auto p-4 '>
@@ -12,12 +35,13 @@ const Navbar = () => {
         </Link>
         <div>
             <ul className='hidden md:flex'>
-                <Link href="/#home">
-                    <li className='ml-10 text-sm uppercase cursor-pointer duration-200 ease-out hover:scale-105 tracking-wider '>home</li>
-                </Link>
-                <Link href="/#home">
-                    <li className='ml-10 text-sm uppercase cursor-pointer duration-200 ease-out hover:scale-105 tracking-wider '>home</li>
-                </Link>
+            {links.map(({id,link})=> (
+                  <Link key={id} href={`/#${link}`}>
+                    <li className='ml-10 text-sm uppercase cursor-pointer duration-200 ease-out hover:scale-105 tracking-wider '>{link}</li>
+                   
+                 </Link> 
+            ))}
+             
             </ul>
         </div>
      </div>
