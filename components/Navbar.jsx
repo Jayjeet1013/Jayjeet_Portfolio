93c909bc1a23fc2d608b8ebf436a3d9363dbb118
Jayjeet1013 <jayjeet1013@gmail.com>
2023-03-03 11:26:57 +0530

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className='w-full h-20 z-10 fixed bg-white text-black duration-300 ease-in'>
+     
+    </div>
+  
+    );
+};
 
 export default Navbar
