cf7c73826d6a83ff52b3e65eff1d6dc82f4bf1db
Jayjeet1013 <jayjeet1013@gmail.com>
2023-03-03 16:14:03 +0530

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,23 @@
+import Link from 'next/link';
 import React from 'react'
 
 const Navbar = () => {
   return (
     <div className='w-full h-20 z-10 fixed bg-white text-black duration-300 ease-in'>
-     
+     <div className='flex justify-between items-center w-full h-full max-w-screen-xl mx-auto p-4 '>
+        <Link href='/#home'>
+        <h1 className='text-3xl lg:text-4xl font-bold uppercase underline underline-offset-2 tracking-wider cursor-pointer'>
+            Jayjeet
+        </h1>
+        </Link>
+        <div>
+            <ul>
+                <Link href="/#home">
+                    <li className='ml-10 text-sm uppercase cursor-pointer duration-200 ease-out'>home</li>
+                </Link>
+            </ul>
+        </div>
+     </div>
     </div>
   
     );
