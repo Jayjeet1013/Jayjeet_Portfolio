54844b16ae530d96a3fc9c7cdc7895e95e3bb1ca
Jayjeet Kumar <jayjeet1013@gmail.com>
2023-03-10 21:10:05 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import HomePage from "../components/HomePage";
 
-const inter = Inter({ subsets: ['latin'] })
+
+
 
 export default function Home() {
   return (
-    <>
-      
-    <div>
-     <h1 className='text-3xl text-red-500'>  </h1>
-    </div>
-    </>
+   <div>
+    <HomePage/>
+   </div>
   )
 }
