e2ae606fdafd1e74747f9d8994b8ef85ff5ac9c4
Jayjeet Kumar <jayjeet1013@gmail.com>
2023-03-11 14:40:12 +0530

    update

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
+import html from '../public/assets/experience/html.png'
+import css from '../public/assets/experience/css.png'
+import github from '../public/assets/experience/github.png'
+import javascript from '../public/assets/experience/javascript.png'
+import node from '../public/assets/experience/node.png'
+import react from '../public/assets/experience/react.png'
+import tailwind from '../public/assets/experience/tailwind.png'
+import next from '../public/assets/experience/next.png'
+
 const Experience = () => {
-  return (
-    <div>Experience</div>
-  )
+  return <div id='experience'>
+<div className='max-w-screen-xl mx-auto px-8 py-16 text-center md:text-left  '>
+                        <h2 className="text-5xl md:text-7xl  tracking-wider uppercase text-blue-500 font-bold" >experience</h2>
+                        <p className='py-4'>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita inventore iure itaque natus facere excepturi nulla voluptates ipsam numquam sequi. Quam accusantium earum cumque!
+                        </p>
+
+    </div>
+  </div>
 }
 
 export default Experience
