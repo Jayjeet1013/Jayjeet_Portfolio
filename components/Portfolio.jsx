2571583b0611c133f3a0f2f9e8322bdeef425501
Jayjeet Kumar <jayjeet1013@gmail.com>
2023-03-11 13:13:39 +0530

    update

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,9 +1,52 @@
+import Link from 'next/link';
 import React from 'react'
+import Image from 'next/image';
+import { MdExpandMore } from 'react-icons/md';
+
+import installNode from '../public/portfolio/installNode.png'
+import reactWeather from '../public/portfolio/reactWeather.png'
+
 
 const Portfolio = () => {
-  return (
-    <div>Portfolio</div>
-  )
+
+ const portfolios = [
+        {
+                id:1,
+                title:"react weather app",
+                imageSrc: reactWeather,
+                url:"react-weather",
+        },
+        {
+                id:2,
+                title:"installNode",
+                imageSrc:installNode,
+                url:"installNode",
+        },
+ ];
+
+        return <div id='portfolio' className='w-full'>
+                <div className='max-w-screen-xl mx-auto px-8 py-16 text-center md:text-left  '>
+                        <h2 className="text-5xl md:text-7xl  tracking-wider uppercase text-blue-500 font-bold" >portfolio</h2>
+                        <p className='py-4'>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita inventore iure itaque natus facere excepturi nulla voluptates ipsam numquam sequi. Quam accusantium earum cumque!
+                        </p>
+                        <div className='max-w-5xl mx-auto grid md:grid-cols-2 gap-8'>
+
+                                {portfolios.map(({id,title,imageSrc,url})=>(
+                                       <Link key={id} href={`/portfolio/${url}`}>
+
+                                       <div className='cursor-pointer group shadow-md shadow-gray-600 overflow-hidden rounded-md'>
+                                         <Image src={imageSrc} alt={title} className='rounded-md'/>
+
+                                       </div>
+
+                                       </Link>
+                                 ))}
+
+                        </div>
+                </div>
+        </div>
+
 }
 
-export default Portfolio
+export default Portfolio;
--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ import Portfolio from "../components/Portfolio";
 export default function Home() {
   return (
    <div>
-    <HomePage/>
-    <Portfolio/>
+    <HomePage />
+    <Portfolio />
    </div>
-  )
+  );
 }
