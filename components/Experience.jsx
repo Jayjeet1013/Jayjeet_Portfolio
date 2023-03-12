99c6e364fb4de4b0734f7c94adbbf0cd4702c2a2
Jayjeet Kumar <jayjeet1013@gmail.com>
2023-03-13 02:56:37 +0530

    update

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 import html from "../public/assets/experience/html.png";
 import css from "../public/assets/experience/css.png";
@@ -8,8 +9,58 @@ import node from "../public/assets/experience/node.png";
 import react from "../public/assets/experience/react.png";
 import tailwind from "../public/assets/experience/tailwind.png";
 import next from "../public/assets/experience/next.png";
+import solidity from "../public/assets/experience/Solidity.png";
 
 const Experience = () => {
+  const experiences = [
+    {
+      id: 1,
+      title: "Html",
+      src: html,
+    },
+    {
+      id: 2,
+      title: "Css",
+      src: css,
+    },
+    {
+      id: 3,
+      title: "Javascript",
+      src: javascript,
+    },
+    {
+      id: 4,
+      title: "Solidity",
+      src: solidity,
+    },
+    {
+      id: 5,
+      title: "Git & Github",
+      src: github,
+    },
+    {
+      id: 6,
+      title: "React",
+      src: react,
+    },
+    {
+      id: 7,
+      title: "Node",
+      src: node,
+    },
+    {
+      id: 8,
+      title: "Next",
+      src: next,
+    },
+   
+    {
+      id: 9,
+      title: "Tailwind",
+      src: tailwind,
+    },
+  ];
+
   return (
     <div id="experience">
       <div className="max-w-screen-xl mx-auto px-8 py-16 text-center md:text-left  ">
@@ -21,6 +72,20 @@ const Experience = () => {
           iure itaque natus facere excepturi nulla voluptates ipsam numquam
           sequi. Quam accusantium earum cumque!
         </p>
+
+        <div className="grid lg:grid-cols-4 gap-8">
+          {experiences.map(({ id, title, src }) => (
+            <div
+              key={id}
+              className="flex flex-col lg:flex-row gap-10 lg:gap-0 items-center justify-between p-6 shadow-lg rounded-xl hover:scale-105 ease-in duration-300 odd:shadow-orange-300 even:shadow-blue-400"
+            >
+
+      <Image src={src} width='64px' height='64px' alt={title} />
+      <h2 className="font-light">{title}</h2>
+  
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
