f3ce69a0cb38fc3ee5a3f956e9ee4e57e09cdd2d
Jayjeet Kumar <jayjeet1013@gmail.com>
2023-03-20 16:08:39 +0530

    update

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import heroImage from '../public/assets/iam.png'
+// import heroImage from '../public/assets/heroImage.png'
 import {MdExpandMore}  from 'react-icons/md'
-import Image from 'next/image'
+// import Image from 'next/image'
 import Link from 'next/link'
+
+
+
 
 const HomePage = () => {
   return (
-    <div  id='home'  className='h-screen w-full text-center'>
-     <div className='max-w-screen-xl mx-auto w-full h-full pt-24 p-4 flex flex-col justify-around items-center'  >
-       <div className='w-64 h-64 mx-auto overflow-hidden rounded-xl'>
+      <div  id='home'  className='h-screen w-full text-center'> 
+    <div className='max-w-screen-xl mx-auto w-full h-full pt-24 p-4 flex flex-col justify-around items-center'  > 
+       {/* <div className='w-64 h-64 mx-auto overflow-hidden rounded-xl'>
         <Image scr={heroImage} alt='me'/>
-       </div>
+       </div> */}
        <h1 className=' uppercase font-bold text-gray-700 text-7xl '>Web3 Developer</h1>
        <p className='text-gray-600  text-xl max-w-sm mx-auto '> I am a  Fronted Developer || Smart contract developer || Web3 Developer </p>
      <Link href="/#me">
@@ -21,8 +24,9 @@ const HomePage = () => {
         </span>
       </div>
      </Link>
-     </div>
-    </div>
+      </div>
+    </div> 
+    
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,11 @@ import Portfolio from "../components/Portfolio";
 
 
 
+
 export default function Home() {
   return (
    <div>
+
     <HomePage />
     <Portfolio />
     <Experience/>
